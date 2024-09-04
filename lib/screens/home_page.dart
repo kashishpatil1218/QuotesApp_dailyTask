1195fc147b75c56e57538b4448e6f448748af4a7
Kashish <patilkashish649@gmail.com>
2024-09-04 15:00:49 +0530

    4-9

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -34,14 +34,129 @@ class _HomePageState extends State<HomePage> {
           style: TextStyle(color: Colors.white, fontSize: 20),
         ),
         actions: [
-          IconButton(onPressed: () {
-            setState(() {
-              isGrid=!isGrid;
-            });
-          }, icon:(isGrid)? Icon(Icons.grid_view,color: Colors.white,):Icon(Icons.list,color: Colors.white,))
+          IconButton(
+              onPressed: () {
+                setState(() {
+                  isGrid = !isGrid;
+                });
+              },
+              icon: (isGrid)
+                  ? Icon(
+                      Icons.grid_view,
+                      color: Colors.white,
+                    )
+                  : Icon(
+                      Icons.list,
+                      color: Colors.white,
+                    ))
         ],
       ),
-      body: (isGrid)? GrideviewMethod() : ListviewMethod(),
+      body: (isGrid) ? GrideviewMethod() : ListviewMethod(),
+      floatingActionButton: Padding(
+        padding: EdgeInsets.only(left: 30),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            ElevatedButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      icon: Icon(Icons.refresh),
+                      title: const Center(
+                        child: Text(
+                          'Reset Settings?',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.w500),
+                        ),
+                      ),
+                      content: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          const Divider(
+                            thickness: 2,
+                          ),
+                          const Text(
+                            'This will reset your app Prefrence back\nTo their default setting. The following\naccounts will also be signed out: ',
+                            style: TextStyle(fontSize: 12),
+                          ),
+                          const Divider(
+                            thickness: 2,
+                          ),
+                          emialAddress(mail: 'jenelia123@gmail.com'),
+                          emialAddress(mail: 'admin345@gmail.com'),
+                          emialAddress(mail: 'smith675@gmail.com'),
+                          const Divider(thickness: 2,),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                             TextButton(onPressed: () {
+
+                             }, child: Text('Cancle')),
+                              TextButton(onPressed: () {
+
+                              }, child: Text('Accept'))
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    backgroundColor: Colors.green.shade300),
+                child: const Text(
+                  'Reset',
+                  style: TextStyle(
+                      color: Colors.black, fontWeight: FontWeight.bold),
+                )),
+            ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    backgroundColor: Colors.green.shade300),
+                child: const Text(
+                  'Icons',
+                  style: TextStyle(
+                      color: Colors.black, fontWeight: FontWeight.bold),
+                )),
+            ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    backgroundColor: Colors.green.shade300),
+                child: const Text(
+                  'Full Screen',
+                  style: TextStyle(
+                      color: Colors.black, fontWeight: FontWeight.bold),
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget emialAddress({required String mail }) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 5),
+      child: Row(
+        children: [
+          CircleAvatar(
+            radius: 25,
+            backgroundColor: Colors.green,
+            backgroundImage: NetworkImage(img1),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.only(left:10),
+            child: Text(mail),
+          )
+        ],
+      ),
     );
   }
 }
--- lib/utils/global.dart
+++ lib/utils/global.dart
@@ -1,3 +1,5 @@
 List  QuosteView=[];
 
 bool isGrid=false;
+
+String img1='https://www.mockofun.com/wp-content/uploads/2019/12/circle-profile-pic.jpg';
